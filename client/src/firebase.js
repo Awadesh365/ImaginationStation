5a9c48684d2d466651b9f1c3a1bb1d323c04f636
Awadesh <36awadesh@gmail.com>
2024-03-19 23:47:19 +0530

    Integrate Google OAuth functionality.

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "imaginationstation-528f1.firebaseapp.com",
+    projectId: "imaginationstation-528f1",
+    storageBucket: "imaginationstation-528f1.appspot.com",
+    messagingSenderId: "731799376715",
+    appId: "1:731799376715:web:770cf37adf90630bc20ece"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 import {
   signInStart,
   signInSuccess,
@@ -92,6 +93,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
