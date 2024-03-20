308d9f06ba587cc01c12b317cf0e69cebb581365
Awadesh <36awadesh@gmail.com>
2024-03-20 19:38:29 +0530

    Enhance header component to display user data.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: meta.env.VITE_FIREBASE_API_KEY,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "imaginationstation-528f1.firebaseapp.com",
     projectId: "imaginationstation-528f1",
     storageBucket: "imaginationstation-528f1.appspot.com",
