f7460ee79f27fb6c01ae7d40d4c111ac3f4a5c73
Awadesh <36awadesh@gmail.com>
2024-05-05 02:16:23 +0530

    Wrap up About and Projects Pages Development.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Imagination Station
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Imagination Station is an open-source project created by Awaadesh
+              and it is a community-driven project.
+            </p>
 
-const About = () => {
-  return <div>About</div>;
-};
+            <p>
+              Imagination Station is a platform that allows users to share their
+              thoughts, ideas, and experiences with other users. We believe that
+              everyone should have access to the same resources.
+            </p>
 
-export default About;
+            <p>
+              Imagination Station is a platform that allows users to share their
+              thoughts, ideas, and experiences with other users. We believe that
+              everyone should have access to the same resources.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
